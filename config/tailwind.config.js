9a7615d064977c80b0ba706974d19793d6ae23d8
Samuel A <sassani134@gmail.com>
2023-02-06 19:14:38 +0100

    Ajout des couleurs gold et celle par default

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -8,6 +9,27 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      gold: {
+        100: "#FFFFB3",/* -70% de l'original*/
+        200: "#FFFF99",/* -60% de l'original*/
+        300: "#FFFF7F",/* -50% de l'original*/
+        400: "#FFFF33",/* -20% de l'original*/
+        500: "#FFD700", /*Couleur original */ 
+        600: "#E6BE00",/* -10% de l'original*/
+        700: "#CCA400",/* -20% de l'original*/
+        800: "#B38B00",/* -30% de l'original*/
+        900: "#997100",/* -40% de l'original*/
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
