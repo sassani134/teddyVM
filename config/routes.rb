5fc2daa33e69d402aced44aa0379c9bb605746f9
Samuel A <sassani134@gmail.com>
2023-02-17 17:58:32 +0100

    Ajout des routes cookies et ageGates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'age_gates/index'
-  get 'cookies/index'
-  get 'cookies/policy'
+  get 'age_gates',to: 'age_gates#index'
+  post "age_gates/verification", to: "age_gates#verification", as: "age_verification"
+
+  get 'cookies', to: 'cookies#index'
+  post "cookies/consent", to: "cookies#consent", as: "cookie_consent"
+  get 'cookies/policy', to: 'cookies#policy', as: 'cookies_policy'
+
+  
   get 'landing/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
