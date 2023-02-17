9b42227ef34a29d581d4521abd27caa457440afa
Samuel A <sassani134@gmail.com>
2023-02-17 17:56:41 +0100

    Cookies age-gates et cookie normal

--- app/controllers/age_gates_controller.rb
+++ app/controllers/age_gates_controller.rb
@@ -2,4 +2,9 @@
 
 class AgeGatesController < ApplicationController
   def index; end
+
+  def verification
+    session[:age_verification] = params[:verification].presence
+    render turbo_stream: turbo_stream.remove(:age_verification)
+  end
 end
--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -3,5 +3,10 @@
 class CookiesController < ApplicationController
   def index; end
 
+  def consent
+    session[:cookie_consent] = params[:consent].presence
+    render turbo_stream: turbo_stream.remove(:cookie_consent)
+  end
+
   def policy; end
 end
