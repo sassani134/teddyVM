8f7767874ff4511e3c74c235510a8b23385e31ea
Samuel A <sassani134@gmail.com>
2023-03-12 21:44:38 +0100

    Ajout de stimulus-scroll-to & stimulus-scroll-reveal

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,6 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "stimulus-scroll-to", to: "https://ga.jspm.io/npm:stimulus-scroll-to@4.1.0/dist/stimulus-scroll-to.mjs"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
+pin "stimulus-scroll-reveal", to: "https://ga.jspm.io/npm:stimulus-scroll-reveal@3.1.0/dist/stimulus-scroll-reveal.mjs"
