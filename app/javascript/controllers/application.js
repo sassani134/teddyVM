070b6c3b256d0996649542e0f53a6f4e93fd2770
Samuel A <sassani134@gmail.com>
2023-03-24 00:09:19 +0100

    Scroll to march

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import ScrollTo from 'stimulus-scroll-to'
+import ScrollReveal from 'stimulus-scroll-reveal'
 
 const application = Application.start()
+application.register("scroll-to", ScrollTo)
+application.register('scroll-reveal', ScrollReveal)
 
 // Configure Stimulus development experience
 application.debug = false
