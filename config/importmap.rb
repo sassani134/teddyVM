bb9f55073fad8f5f48c29ede7494d2c33ab11c9d
Samuel <sassani134@gmail.com>
2023-07-05 22:10:46 +0200

    Adding Fontawesome

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,4 @@ pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
 pin "stimulus-scroll-to", to: "https://ga.jspm.io/npm:stimulus-scroll-to@4.1.0/dist/stimulus-scroll-to.mjs"
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
 pin "stimulus-scroll-reveal", to: "https://ga.jspm.io/npm:stimulus-scroll-reveal@3.1.0/dist/stimulus-scroll-reveal.mjs"
+pin '@fortawesome/fontawesome-free', to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js'
